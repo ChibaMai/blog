6d7d4c8dbcabd7155460b454a95468e21309e294
Thomasvader <lwmwll@163.com>
2021-04-10 18:16:05 +0800

    Added. Github workflow.

---
+++ _nuxt/static/1618049641/widget/ContactMe/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/widget/ContactMe", (function(a){return {data:[{article:{slug:"ContactMe",title:"联系我",toc:[],body:{type:"root",children:[]},dir:"\u002Fwidget",path:"\u002Fwidget\u002FContactMe",extension:".md",createdAt:a,updatedAt:a}}],fetch:{},mutations:void 0}}("2021-04-09T05:29:29.651Z")));
---
+++ _nuxt/static/1618049641/widget/Portfolio/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618049641",layout:"default",error:a,serverRendered:b,routePath:"\u002Fwidget\u002FPortfolio",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a3bb9631"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
