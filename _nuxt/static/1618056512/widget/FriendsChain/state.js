bccb1f1f07d80a3e2cf75ad9cca42be427fe4ee4
Thomasvader <lwmwll@163.com>
2021-04-10 20:09:33 +0800

    addgoogle-site-verification

---
+++ _nuxt/static/1618056512/widget/FriendsChain/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618056512",layout:"default",error:a,serverRendered:b,routePath:"\u002Fwidget\u002FFriendsChain",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"76301c78"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
