b6b785af5a5a8f802ca85c6f60f58825bb359d98
Thomasvader <lwmwll@163.com>
2021-04-16 15:13:15 +0800

    Modify the list of all article bugs, and add the update time of the updated article.

---
+++ _nuxt/static/1618557151/widget/ContactMe/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618557151",layout:"default",error:a,serverRendered:b,routePath:"\u002Fwidget\u002FContactMe",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"567a6a6e"}},colorMode:{preference:c,value:c,unknown:b,forced:false}}}(null,true,"system"));
---
+++ _nuxt/static/1618557151/widget/Portfolio/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/widget/Portfolio", {data:[{article:{slug:"Portfolio",title:"作品集",toc:[],body:{type:"root",children:[]},dir:"\u002Fwidget",path:"\u002Fwidget\u002FPortfolio",extension:".md",createdAt:"2021-04-16T06:25:27.417Z",updatedAt:"2021-04-16T06:25:27.418Z"}}],fetch:{},mutations:void 0});
