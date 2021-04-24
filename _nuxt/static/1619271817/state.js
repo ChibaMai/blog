b35ca270f7332de327914d78769f912316c1d79f
Thomasvader <lwmwll@163.com>
2021-04-24 21:44:16 +0800

    Add new articles, add Markdown-TOC, fix bugs.

---
+++ _nuxt/static/1619271817/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1619271817",layout:"default",error:a,serverRendered:b,routePath:c,config:{app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"87b5b4e7"}},colorMode:{preference:d,value:d,unknown:b,forced:false}}}(null,true,"\u002F","system"));
---
+++ _nuxt/static/1619271817/widget/ContactMe/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/widget/ContactMe", {data:[{article:{slug:"ContactMe",title:"联系我",toc:[],body:{type:"root",children:[]},dir:"\u002Fwidget",path:"\u002Fwidget\u002FContactMe",extension:".md",createdAt:"2021-04-24T05:42:16.639Z",updatedAt:"2021-04-24T05:42:16.640Z"}}],fetch:{},mutations:void 0});
