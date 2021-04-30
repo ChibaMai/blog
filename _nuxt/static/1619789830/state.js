380dbed61f579f4e9429087f21c6cdf4031b938f
Thomasvader <lwmwll@163.com>
2021-04-30 21:38:56 +0800

    New search function (only search title, content is for search setting), fix some bugs.

---
+++ _nuxt/static/1619789830/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1619789830",layout:"default",error:a,serverRendered:b,routePath:c,config:{app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"553282a4"}},colorMode:{preference:d,value:d,unknown:b,forced:false}}}(null,true,"\u002F","system"));
