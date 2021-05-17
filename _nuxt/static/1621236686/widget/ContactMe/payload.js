207083a1ccf8f97d33a7f15a88892d755dd99c04
Thomasvader <lwmwll@163.com>
2021-05-17 15:32:03 +0800

    Use front-end indexedDB for data storage. New modules, updates, and queries have been completed, and some bugs have been fixed.

---
+++ _nuxt/static/1621236686/widget/ContactMe/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/widget/ContactMe", (function(a){return {data:[{article:{slug:"ContactMe",title:"联系我",toc:[],body:{type:"root",children:[]},dir:"\u002Fwidget",path:"\u002Fwidget\u002FContactMe",extension:".md",createdAt:a,updatedAt:a}}],fetch:{},mutations:void 0}}("2021-05-16T15:16:13.367Z")));
